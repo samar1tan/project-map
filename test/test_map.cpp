70dd3910412c6ef8ace66975400707d8651ef343
Jingzhi Zhang <zjz42027@gmail.com>
2018-12-21 12:14:56 +0800

    bulid simple test for SimpleMap and Entry

--- test/test_map.cpp
+++ test/test_map.cpp
@@ -3,14 +3,30 @@
 #include <string>
 using namespace std;
 
-int main() {
-    SimpleMap<int, string>* test = new SimpleMap<int, string>;
-    test->insert(Entry<int, string>(42027, "Zhang Jingzhi"));
-    
-    cout << (*test)[42027] << endl;
+// typedef for test
+typedef SimpleMap<int, string> Map;
+typedef Entry<int, string> Elem;
+typedef SimpleMap<string, int> rMap;
+typedef Entry<string, int> rElem;
 
-    delete test;
+// test SimpleMap and also Entry
+int main() {
+    // Construct
+    Map* test1 = new Map;
+    rMap test2;
+    
+    // Process
+    test1->insert(Elem(42027, "Zhang Jingzhi"));
+    test2.insert(rElem("China Mobile", 10086));
+
+    // Output
+    cout << "42027 is stu id of " << (*test1)[42027] << endl;
+    cout << "42027 is stu id of " << *(test1->max()) << endl;
+    cout << "China Mobile's TEL: " << test2["China Mobile"] << endl;
+    cout << "max size of SimpleMap is " << test1->max_size() << endl;
+
+    // Destruct
+    delete test1;
 
     system("pause");
-    return 0;
 }
